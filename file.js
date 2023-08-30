76318b4404aa9504bf0dfa85085daaddee96e1b7
zain <mzainkhan2005@gmail.com>
2023-08-30 20:40:16 +0500

    second commit

---
+++ file.js
@@ -0,0 +1,41 @@
+const catergories = document.querySelectorAll('.categorie');
+const productBuyItemButtons = document.querySelectorAll(
+  '.product-gallery-item'
+);
+const btncng = document.querySelector('.action-signup');
+const removeimg = document.querySelector('.remove-img');
+let cardBadgeCount = 2;
+
+catergories.forEach((item) => {
+  item.querySelector('.categorie-name').addEventListener('click', () => {
+    if (item.classList.contains('active')) {
+      item.classList.remove('active');
+    } else {
+      closeAllCategroies();
+      item.classList.toggle('active');
+    }
+  });
+});
+
+productBuyItemButtons.forEach((button) => {
+  console.log(button);
+
+  button.addEventListener('click', () => {
+    console.log('click');
+    cardBadgeCount++;
+    console.log(cardBadgeCount);
+  });
+});
+
+function closeAllCategroies() {
+  catergories.forEach((item) => {
+    item.classList.remove('active');
+  });
+}
+
+btncng.addEventListener('click', function () {
+  document.querySelector('.login-card').classList.remove('hidden');
+});
+removeimg.addEventListener('click', function () {
+  document.querySelector('.login-card').classList.add('hidden');
+});
