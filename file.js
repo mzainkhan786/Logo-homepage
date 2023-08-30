5a0f1945cd0615534e2204ce68201fc8a1d91fd1
zain <mzainkhan2005@gmail.com>
2023-08-30 21:28:08 +0500

    third commit

--- file.js
+++ file.js
@@ -35,9 +35,3 @@ btncng.addEventListener('click', function () {
 removeimg.addEventListener('click', function () {
   document.querySelector('.login-card').classList.add('hidden');
 });
-// let total = 0;
-// for (let i = 0; i <= 11; i++) {
-//   // console.log(i);
-//   total += i;
-// }
-// console.log(total);
